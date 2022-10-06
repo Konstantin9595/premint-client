d5bf570d3a703b0b426d9ffc5313e1e068a3c96f
konstantins9595 <konstantins9595@gmail.com>
2022-10-06 15:28:53 +0600

    add cycletls for vercel api function

--- api/check-url.js
+++ api/check-url.js
@@ -1,31 +1,45 @@
-const PremintClient = require('../PremintClient.js').default
-export default function handler(req, res) {
+const PremintClient = require('../PremintClient').default
 
-    const {ruffleUrl} = req.body
-    if(!ruffleUrl) {
-        const responseData = JSON.stringify({
-            responseStatus: 400,
-            message: 'Invalid request. RuffleUrl not found'
-        })
+const client = new PremintClient()
+client.checkRuffleUrl('https://www.premint.xyz/piratesnft')
+.then((respData) => {
+    console.log('respData: ', respData)
+    //const responseStatus = respData.responseStatus !== 200 ? 400 : 200
+    //const responseData = JSON.stringify({responseStatus})
 
-        res.status(400).json(responseData)
-        return false
-    }
+    // res.status(responseData).json(responseData)
+})
+.finally(() => {
+    client.tlsInstance.then(res => res.exit())
+})
 
-    const client = new PremintClient()
-    client.checkRuffleUrl(ruffleUrl)
-    .then((respData) => {
-        const responseStatus = respData.responseStatus !== 200 ? 400 : 200
-        const responseData = JSON.stringify({responseStatus})
+// export default function handler(req, res) {
 
-        res.status(responseData).json(responseData)
-    })
-    .catch((err) => {
-        const responseData = JSON.stringify({responseStatus: 404, message: err.message})
-        res.status(404).json(responseData)
-    })
-    .finally(() => {
-        client.tlsInstance.then((tls) => tls.exit().then(() => console.log('tls process closed.')))
-    })
+//     const {ruffleUrl} = req.body
+//     if(!ruffleUrl) {
+//         const responseData = JSON.stringify({
+//             responseStatus: 400,
+//             message: 'Invalid request. RuffleUrl not found'
+//         })
 
-  }
+//         res.status(400).json(responseData)
+//         return false
+//     }
+
+//     const client = new PremintClient()
+//     client.checkRuffleUrl(ruffleUrl)
+//     .then((respData) => {
+//         const responseStatus = respData.responseStatus !== 200 ? 400 : 200
+//         const responseData = JSON.stringify({responseStatus})
+
+//         res.status(responseData).json(responseData)
+//     })
+//     .catch((err) => {
+//         const responseData = JSON.stringify({responseStatus: 404, message: err.message})
+//         res.status(404).json(responseData)
+//     })
+//     .finally(() => {
+//         client.tlsInstance.then((tls) => tls.exit().then(() => console.log('tls process closed.')))
+//     })
+
+//   }
